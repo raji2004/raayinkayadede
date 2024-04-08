4b80ad4b43cd614a7b2e14b0bfda4686aaaa6750
raji <kingraj1344@gmail.com>
2024-04-08 21:50:49 +0100

    Refactor ongoing and upcoming project layouts, add Project component, update styles and index.js

--- src/pages/SeeMore/seeMore.jsx
+++ src/pages/SeeMore/seeMore.jsx
@@ -1,10 +1,30 @@
 import React from "react";
+import { Navigation } from "../../components";
 import Style from "./seeMore.module.css";
+import Image1 from "../../assets/images/IMG-20230915-WA0007.jpg";
+import Image2 from "../../assets/images/IMG-20230915-WA0009.jpg";
+import Image3 from "../../assets/images/IMG-20230822-WA0013.jpg";
+import Image4 from "../../assets/images/IMG-20230915-WA0010.jpg";
+import { Slide } from 'react-slideshow-image';
 
+const slideImg = [
+    Image1,
+    Image2,
+    Image3,
+    Image4
+]
 export const SeeMore = () => {
-    return(
-        <div>
-            f
-        </div>
+    return (
+        <>
+            <Navigation />
+            <Slide>
+               <img src={Image1} alt="slide1" />
+               <img src={Image1} alt="slide1" />
+               <img src={Image1} alt="slide1" />
+               <img src={Image1} alt="slide1" />
+            </Slide>
+        </>
+
+
     )
 }
